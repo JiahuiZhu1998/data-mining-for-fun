ded604ea5d0a31051201921d7f3338cd99c76e8d
ZhuzhuxiaBABY <37950026+ZhuzhuxiaBABY@users.noreply.github.com>
2019-05-21 23:09:12 -0400

    try commit

--- 005. pandas code intro/intro_pandas_4.py
+++ 005. pandas code intro/intro_pandas_4.py
@@ -100,3 +100,67 @@ Daily.loc['FL']['2012':].plot()
 Daily.loc['GA']['2012':].plot()
 Daily.loc['NY']['2012':].plot()
 Daily.loc['TX']['2012':].plot()
+
+StateYearMonth = Daily.groupby([Daily.index.get_level_values(0),Daily.index.get_level_values(1).year,Daily.index.get_level_values(1).month])
+Daily['Lower'] = StateYearMonth['CustomerCount'].transform(lambda x:x.quantile(q=.25)-(1.5*x.quantile(q=.75)-x.quantile(q=.25)))
+Daily['Upper'] = StateYearMonth['CustomerCount'].transform(lambda x:x.quantile(q=.75)+(1.5*x.quantile(q=.75)-x.quantile(q=.25)))
+Daily['Outlier'] = (Daily['CustomerCount']<Daily['Lower']) | (Daily['CustomerCount']>Daily['Upper'])
+###
+Daily=Daily[Daily['Outlier']==False]
+print(Daily.head())
+
+###Combine all markets
+ALL =pd.DataFrame(Daily['CustomerCount'].groupby(Daily.index.get_level_values(1)).sum())
+ALL.columns = ['CustomerCount']
+#Group by Year and Month
+YearMonth=ALL.groupby([lambda x:x.year, lambda x:x.month])
+# What is the max customer count per Year and Month
+ALL['Max'] = YearMonth['CustomerCount'].transform(lambda x:x.max())
+print(ALL.head())
+
+#######################
+data = [1000,2000,3000]
+idx = pd.date_range(start='12/31/2011',end='12/31/2013',freq='A')
+BHAG = pd.DataFrame(data,index=idx,columns=['BHAG'])
+print('\n')
+print(BHAG)
+#######################
+combined = pd.concat([ALL,BHAG], axis=0)
+combined = combined.sort_index(axis=0)
+print('\n')
+print(combined.tail())
+#######################
+fig,axes = plt.subplots(figsize=(12,7))
+
+combined['BHAG'].fillna(method='pad').plot(color='green',label='BHAG')
+combined['Max'].plot(color='blue',label='All Markets')
+plt.legend(loc='best')
+plt.show()
+print('\n')
+###Group by Year and then get the max value per year
+Year = combined.groupby(lambda x:x.year).max()
+print(Year)
+print('\n')
+##Add a column representing the percent change per year
+Year['YR_PCT_Change']= Year['Max'].pct_change(periods=1)
+print(Year)
+print('\n')
+#####
+print( (1+Year.loc[2012,'YR_PCT_Change'])*Year.loc[2012,'Max'])
+
+####present data
+ALL['Max'].plot(figsize=(10,5));plt.title('All Markets')
+#Last four Graphs
+fig,axes = plt.subplots(nrows=2,ncols=2,figsize=(20,10))
+fig.subplots_adjust(hspace=1.0)##Create space between plots
+
+Daily.loc['FL']['CustomerCount']['2012':].fillna(method='pad').plot(ax=axes[0,0])
+Daily.loc['GA']['CustomerCount']['2012':].fillna(method='pad').plot(ax=axes[0,1])
+Daily.loc['TX']['CustomerCount']['2012':].fillna(method='pad').plot(ax=axes[1,0])
+Daily.loc['NY']['CustomerCount']['2012':].fillna(method='pad').plot(ax=axes[1,1])
+
+##Add titles
+axes[0,0].set_title('Florida')
+axes[0,1].set_title('Gerogia')
+axes[1,0].set_title('Texas')
+axes[1,1].set_title('North East')
