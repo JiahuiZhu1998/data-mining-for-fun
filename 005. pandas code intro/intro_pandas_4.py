54798aa69c4f2ae2f3ad2c048ebabae50d7f95fb
ZhuzhuxiaBABY <37950026+ZhuzhuxiaBABY@users.noreply.github.com>
2019-05-22 03:59:28 -0400

    try commit

--- 005. pandas code intro/intro_pandas_4.py
+++ 005. pandas code intro/intro_pandas_4.py
@@ -1,17 +1,20 @@
 ################################################################################################
-#Python version3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)]
-#Pandas version0.24.2
-#Matplotlib version3.0.3
-
+# Python version3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)]
+# Pandas version0.24.2
+# Matplotlib version3.0.3
+# Name:Jiahui Zhu
+# Date: 5/22/2019
+# from https://nbviewer.jupyter.org/urls/bitbucket.org/hrojas/learn-pandas/raw/master/lessons/03%20-%20Lesson.ipynb
 # remember to pip install openpyxl
 # remember to pip install xlrd
+## do not understand: Daily = df2.reset_index().groupby(['State','StatusDate']).sum()
+##
 ################################################################################################
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy.random as np
 import sys
 import matplotlib
-import xlrd
 
 #%matplotlib inlines
 print('Python version'+sys.version)
@@ -24,78 +27,101 @@ np.seed(111)
 
 def createDataSet(Number=1):
     output = []
+    ###randint() can create random numbers
     for i in range(Number):
         rng = pd.date_range(start='1/1/2009',end='12/31/2012',freq='W-MON')##create weekly(mondays)date range
-        date = np.randint(low=25,high=1000,size=len(rng))##create random date
-        #print(date)
+        date = np.randint(low=25,high=1000,size=len(rng))##create random integer number
         status = [1,2,3] ## status pool
         random_status = [status[np.randint(low=0,high=len(status))] for i in range(len(rng))]## make a random list of status
-        states = ['GA','FL','fl','NY','NY','TX'] ## state pool
+        states = ['GA','FL','fl','NY','NJ','TX'] ## state pool
         random_states = [states[np.randint(low=0,high=len(states))] for i in range(len(rng))]
+        ## extend means no bruckets. it will combine elements in the list into another list
         output.extend(zip(random_states,random_status,date,rng))
-
+        #print(output)
         return output
 
 dataset = createDataSet(4)
-#print(dataset)
 df1 = pd.DataFrame(data=dataset, columns=['State','Status','CustomerCount','StatusDate'])
 print('\n')
+### df1.info is different from instructions
 print(df1.info())
 print('\n')
 print(df1.head())
+
+
 
 ## save results to excel
 df1.to_excel('Lesson3.xlsx',index=False)
 print('\n')
 print('Done')
-
+print('\n')
+print('Part1 has ended!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 ##grab data from excel
 location = r'C:/Users/garry/Documents/data-mining-for-fun/005. pandas code intro/Lesson3.xlsx'
+### read_excel(io, sheetname=0, header=0, skiprows=None, skip_footer=0, index_col=None,names=None, parse_cols=None, parse_dates=False,date_parser=None,na_values=None,thousands=None, convert_float=True, has_index_names=None, converters=None,dtype=None, true_values=None, false_values=None, engine=None, squeeze=False, **kwds)
 df2 = pd.read_excel(location,0,index_col='StatusDate')
 print('\n')
 print(df2.dtypes)
 print('\n')
+### show all 'StatusDate' in a list
 print(df2.index)
 print('\n')
 print(df2.head())
 print('\n')
+print('Start to prepare data!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+print('\n')
+#### see how many different states appears in the dataframe
 print(df2['State'].unique())
 print('\n')
+### change all lower cases to upper case and display the list again
 df2['State'] = df2.State.apply(lambda x: x.upper())
 print(df2['State'].unique())
 print('\n')
-mask = df2['Status']==1
-df2 = df2[mask]
-
+############only when Status is equal to 1 and grab
+df2 = df2[df2['Status']==1]
+#print(df2)
+print('\n')
 #Convert NJ to NY
-mask = df2.State =='NJ'
-df2['State'][mask] = 'NY'
+df2['State'][df2.State =='NJ'] = 'NY'
 print(df2['State'].unique())
 print('\n')
+### the figure which is (15,5) is not same as instruction
 df2['CustomerCount'].plot(figsize=(15,5))
 plt.show()
+#print(df2[df2['State']=='NY'])
+### sortdf is not same as instruction's
+#DataFrame.sort_index(axis=0, level=None, ascending=True, inplace=False, kind='quicksort', na_position='last', sort_remaining=True, by=None)
 sortdf = df2[df2['State']=='NY'].sort_index(axis=0)
 print(sortdf.head(10))
 print('\n')
 
 ##Group by State and StatusDate
+print(df2.reset_index())
+## the result is not same as instruction
 Daily = df2.reset_index().groupby(['State','StatusDate']).sum()
 print(Daily.head())
+print('\n')
+print('start to delete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 print('\n')
 del Daily['Status']
 print(Daily.head())
 print('\n')
 print(Daily.index)
 print('\n')
-print(Daily.index.levels[0])
+print(Daily.index.levels[0])### State which is same as instruction
 print('\n')
-print(Daily.index.levels[1])
+print(Daily.index.levels[1]) ###StatusDate same as instruction
 print('\n')
+#### these four plots are not same as instructions
+# print(Daily.loc['FL'])
+# print('\n')
+# print(Daily.loc['GA'])
 Daily.loc['FL'].plot()
 Daily.loc['GA'].plot()
 Daily.loc['NY'].plot()
 Daily.loc['TX'].plot()
 plt.show()
+#### these four plots are not same as instructions
 Daily.loc['FL']['2012':].plot()
 Daily.loc['GA']['2012':].plot()
 Daily.loc['NY']['2012':].plot()
@@ -108,7 +134,7 @@ Daily['Outlier'] = (Daily['CustomerCount']<Daily['Lower']) | (Daily['CustomerCou
 ###
 Daily=Daily[Daily['Outlier']==False]
 print(Daily.head())
-
+"""
 ###Combine all markets
 ALL =pd.DataFrame(Daily['CustomerCount'].groupby(Daily.index.get_level_values(1)).sum())
 ALL.columns = ['CustomerCount']
@@ -164,3 +190,4 @@ axes[0,0].set_title('Florida')
 axes[0,1].set_title('Gerogia')
 axes[1,0].set_title('Texas')
 axes[1,1].set_title('North East')
+"""
---
+++ 005. pandas code intro/intro_pandas_5.py
@@ -0,0 +1,78 @@
+####################################################################################################################
+#  Python version3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)]
+#  Pandas version0.24.2
+#  Matplotlib version3.0.3
+#  Name: Jiahui Zhu
+#  some questions need to comprehensive below:
+#  from https://nbviewer.jupyter.org/urls/bitbucket.org/hrojas/learn-pandas/raw/master/lessons/04%20-%20Lesson.ipynb
+#  from https://blog.csdn.net/starter_____/article/details/79183733
+#  from https://blog.csdn.net/xueruixuan/article/details/81451690
+#  from https://blog.csdn.net/AnneQiQi/article/details/71057069
+#
+#####################################################################################################################
+import pandas as pd
+import sys
+print('Python version:'+sys.version)
+print('Pandas version:'+pd.__version__)
+
+# how to use extend
+list1 = [1,2]
+list2 = [3,4]
+list3 = [];list4=[]
+list3.append(list1)
+list3.append(list2)
+list4.extend(list1)
+list4.extend(list2)
+print(list3)
+print(list4)
+
+# start lesson  pandas tutorial here
+d1 = [0,1,2,3,4,5,6,7,8,9]
+df1 = pd.DataFrame(d1)
+print(df1)
+## assign name to the first column of d1
+df1.columns = ['Rev']
+print(df1)
+## add a new column to df1
+df1['NewCol'] = 5
+print(df1)
+print('\n')
+## modify new column
+df1['NewCol'] = df1['NewCol']+1
+print(df1)
+print('\n')
+## then we can delete columns
+del df1['NewCol']
+print(df1)
+print('\n')
+## lets add a couple of columns
+df1['test'] = 3
+df1['col'] = df1['Rev']
+print(df1)
+print('\n')
+##if we wanted, we could change the name of the index
+i =['a','b','c','d','e','f','g','h','i','j']
+df1.index = i
+print(df1)
+print('\n')
+print(df1.loc['a'])
+print('\n')
+print(df1.loc['a':'d'])
+print('\n')
+print(df1.iloc[0:3])## iloc and loc have been included in previous python script
+print('\n')
+print(df1['Rev'])
+print('\n')
+print(df1[['Rev','test']])## need to remember use  two bruckets
+print('\n')
+print(df1.index[0:3])
+print(df1.loc[df1.index[0:3],'Rev'])
+print('\n')
+print(df1.loc[df1.index[5:],'col'])
+print('\n')
+print(df1.loc[df1.index[:3],['col','test']])
+print('\n')
+print(df1.head())
+print('\n')
+print(df1.tail())
+print('\n')
