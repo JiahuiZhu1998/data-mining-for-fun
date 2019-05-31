8ed138b3c29c609b436e80c90e813383460c08a1
ZhuzhuxiaBABY <37950026+ZhuzhuxiaBABY@users.noreply.github.com>
2019-05-31 03:19:03 -0400

    try commit

---
+++ 008. time code intro/time code intro 1.py
@@ -0,0 +1,9 @@
+'''
+use time and calendar module
+ from https://www.runoob.com/python/python-date-time.html
+ from https://docs.python.org/3/library/time.html
+ from https://blog.csdn.net/BobAuditore/article/details/79377679
+
+
+'''
+import time
