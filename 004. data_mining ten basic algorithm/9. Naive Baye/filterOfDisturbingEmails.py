253241dd7530d2acb23cbfa2d97f6d63d00caba3
ZhuzhuxiaBABY <37950026+ZhuzhuxiaBABY@users.noreply.github.com>
2019-06-04 04:47:20 -0400

    try commit

--- 004. data_mining ten basic algorithm/9. Naive Baye/filterOfDisturbingEmails.py
+++ 004. data_mining ten basic algorithm/9. Naive Baye/filterOfDisturbingEmails.py
@@ -2,6 +2,7 @@
 Name: Jiahui Zhu
 Date: 6/4/2019
 
+sometimes running this file you will get error rate = 0.0, sometimes you get error rate = 0.1
 '''
 import re
 from numpy import *
@@ -9,27 +10,28 @@ from bayes_JiahuiZhu import createVocabList
 from bayes_JiahuiZhu import bagOfWords2VecMN
 from bayes_JiahuiZhu import classifyNB
 from bayes_JiahuiZhu import trainNB0
+
 def textParse(bigString):  # input is big string, #output is word list
     import re
-    listOfTokens = re.split(r'\W*', bigString)
+    listOfTokens = re.split(r'\W+', bigString)
     return [tok.lower() for tok in listOfTokens if len(tok) > 2]
-
 
 def spamTest():
     docList = [];
     classList = [];
     fullText = []
     for i in range(1, 26):
-        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        wordList = textParse(open('file from book/email/spam/%d.txt' % i).read())
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(1)
-        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        wordList = textParse(open('file from book/email/ham/%d.txt' % i).read())
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
     vocabList = createVocabList(docList)  # create vocabulary
-    trainingSet = range(50);
+
+    trainingSet = list(range(50));
     testSet = []  # create test set
     for i in range(10):
         randIndex = int(random.uniform(0, len(trainingSet)))
@@ -46,10 +48,8 @@ def spamTest():
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
         if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
             errorCount += 1
-            print
-            "classification error", docList[docIndex]
-    print
-    'the error rate is: ', float(errorCount) / len(testSet)
+            print("classification error", docList[docIndex])
+    print('the error rate is: ', float(errorCount) / len(testSet))
     # return vocabList,fullText
 
 
@@ -68,3 +68,5 @@ if __name__=='__main__':
 
     emailText = open('file from book/email/ham/6.txt').read()
     listOfTokens = regEx.split(emailText)
+    ## start to use function below:
+    spamTest()
