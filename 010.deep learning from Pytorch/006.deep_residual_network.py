f2407664322cf6e10fadb6f1f1d9b740e6e42b6e
JiahuiZhu1998 <zhuzhuxia555555@gmail.com>
2019-07-03 15:55:58 +0800

    first try

--- 010.deep learning from Pytorch/006.deep_residual_network.py
+++ 010.deep learning from Pytorch/006.deep_residual_network.py
@@ -3,3 +3,30 @@ Name: Jiahui Zhu
 Date: 7/2/2019
 '''
 
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transform
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+num_epochs = 80
+learning_rate = 0.001
+
+transform = transform.Compose([
+    transform.Pad(4),
+    transform.RandomHorizontalFlip(),
+    transform.RandomCrop(32),
+    transform.ToTensor()
+])
+
+train_dataset=torchvision.datasets.CIFAR10(root='../../data/',train=True, transform=transform,download=True)
+test_dataset=torchvision.datasets.CIFAR10(root='../../data/',train=False,transform=transform.ToTensor())
+train_loader=torchvision.utils.data.loader(dataset=train_dataset,batch_size=100,shuffle=True)
+test_loader=torchvision.utils.data.loader(dataset=test_dataset,batch_size=100,shuffle=False)
+
+def conv3x3(in_channel,out_channel,stride=1):
+    return nn.Conv2d(in_channel,out_channel,kernel_size=3,stride=stride,padding=1,bias=False)
+
+class ResidualBlock():
+    pass
