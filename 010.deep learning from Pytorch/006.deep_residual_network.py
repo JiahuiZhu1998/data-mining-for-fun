552dd8f1d3c7c9fd9ea08934727c14bb04c4c38e
JiahuiZhu1998 <zhuzhuxia555555@gmail.com>
2019-07-03 16:09:57 +0800

    first try

--- 010.deep learning from Pytorch/006.deep_residual_network.py
+++ 010.deep learning from Pytorch/006.deep_residual_network.py
@@ -28,5 +28,26 @@ test_loader=torchvision.utils.data.loader(dataset=test_dataset,batch_size=100,sh
 def conv3x3(in_channel,out_channel,stride=1):
     return nn.Conv2d(in_channel,out_channel,kernel_size=3,stride=stride,padding=1,bias=False)
 
-class ResidualBlock():
-    pass
+class ResidualBlock(nn.Module):
+    def __init__(self,in_channels,out_channels,stride=1,downSample=None):
+        super(ResidualBlock,self).__init__()
+        self.conv1 = conv3x3(in_channels,out_channels,stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(in_channels,out_channels)
+        self.bn2 = nn.BatchNorm2(out_channels)
+        self.downSample = downSample
+
+    def forward(self,x):
+        residue = x
+        out = self.conv1(x)
+        out = self.bn1(x)
+        out = self.relu(x)
+        out = self.conv2(x)
+        out = self.bn2(x)
+        if self.downSample:
+            residue = self.downSample(x)
+            out += residue
+            out = self.relu(out)
+            return out
+
